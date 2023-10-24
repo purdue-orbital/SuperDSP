e598944d510d83441aff80106d7674ab4e5fe473
MalinkyZubr <ahumanbeing189@gmail.com>
2023-10-23 20:18:20 -0400

    fftshift test

---
+++ tests/fftshift.rs
@@ -0,0 +1,16 @@
+use num_complex::Complex;
+
+use rustdsp::common::fftshift::fftshift;
+
+#[test]
+fn fftshifttest() {
+    let mut test_data: Vec<Complex<f32>> = Vec::new();
+
+    for i in 0..100 {
+        test_data.push(Complex::new(i as f32, i as f32));
+    }
+
+    let shifted_data: Vec<Complex<f32>> = fftshift(&mut test_data);
+
+    assert!(test_data[test_data.len() - 1] == shifted_data[test_data.len() / 2 - 1]);
+}
