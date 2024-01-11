1cbc837beeac8d32a4bd35c5bfd8d43fda409879
Nicholas Ball <89214070+Nicholas-Ball@users.noreply.github.com>
2024-01-10 22:10:50 -0500

    Worked on feature switching

--- src/math/builder.rs
+++ src/math/builder.rs
@@ -13,28 +13,40 @@ lazy_static!(
 
 /// This builder is a wrapper class around the CPU and GPU compute method that will dynamiclly switch depending on the
 /// feature set
+#[cfg(not(feature = "vulkan"))]
 pub struct WorkflowBuilder<'a> {
-    #[cfg(feature = "vulkan")]
-    vulkan_builder: VulkanCommandBuilder,
-
-    #[cfg(not(feature = "vulkan"))]
     cpu_builder: CPUCommandBuilder<'a>
 }
 
-impl<'a> Default for WorkflowBuilder<'a>{
+#[cfg(feature = "vulkan")]
+pub struct WorkflowBuilder {
+    vulkan_builder: VulkanCommandBuilder
+}
+
+
+#[cfg(feature = "vulkan")]
+impl Default for WorkflowBuilder{
     fn default() -> Self {
 
         WorkflowBuilder{
             #[cfg(feature = "vulkan")]
             vulkan_builder: VULKAN.create_command_builder(),
-            
-            #[cfg(not(feature = "vulkan"))]
-            cpu_builder: CPUCommandBuilder::default()
-
         }
     }
 }
 
-impl<'a> WorkflowBuilder<'a> {
+#[cfg(not(feature = "vulkan"))]
+impl<'a> Default for WorkflowBuilder<'a>{
+    fn default() -> Self {
+        WorkflowBuilder{
+            cpu_builder: CPUCommandBuilder::default(),
+        }
+    }
+}
 
+#[cfg(not(feature = "vulkan"))]
+impl<'a> WorkflowBuilder<'a>{
+    pub fn pointwise_multiply_f32(&self){
+
+    }
 }
