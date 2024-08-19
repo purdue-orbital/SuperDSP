b28fa08c0fbd5113f09d0c190868c9b23e5b3588
Nicholas Ball <89214070+Nicholas-Ball@users.noreply.github.com>
2024-08-19 13:41:01 -0400

    Updated Nix Shell

--- shell.nix
+++ shell.nix
@@ -2,9 +2,20 @@
 pkgs.mkShell{
 	buildInputs = with pkgs; [
 		gcc
-	];
+		rustc
+		cargo
+		pkg-config
+		fontconfig
 
-	nativeBuildInputs = with pkgs; [
-		#pkg-config
+        libxkbcommon
+        libGL
+        wayland
 	];
+	LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [
+        pkgs.rustc
+        pkgs.cargo
+        pkgs.libxkbcommon
+        pkgs.libGL
+        pkgs.wayland
+    ]}";
 }
