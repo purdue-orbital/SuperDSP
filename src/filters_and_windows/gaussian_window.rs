a0a2664b3d16cdc1456fc6eb82b34315f26bb1f0
amshuv08 <vundi.amshu@gmail.com>
2024-09-11 21:01:42 -0400

    gaussian_window

---
+++ src/filters_and_windows/gaussian_window.rs
@@ -0,0 +1,20 @@
+/// Creates a gaussian filter of size N
+/// Formula: w(x) = exp(-0.5 * ((x - N / 2) / (σ * N / 2))^2)
+/// 
+/// # Arguments
+/// N: usize - Size of the filter
+/// sigma: f32 - Standard deviation, controls the width of the Gaussian
+/// 
+/// # Returns
+/// [f32; N] - Gaussian window
+pub fn gaussian_window<const N: usize>(sigma: f32) -> [f32; N] {
+    let mut filter = [0.0; N];
+    let mid = N as f32 / 2.0;
+    
+    for x in 0..N {
+        let norm_x = (x as f32 - mid) / (sigma * mid);
+        filter[x] = (-0.5 * norm_x * norm_x).exp();
+    }
+    
+    filter
+}
---
+++ tests/gaussian_window.rs
@@ -0,0 +1,39 @@
+use superdsp::filters_and_windows::gaussian_window;
+
+const N_SHORT: usize = 16;
+const N_LONG: usize = 64;
+const SIGMA: f32 = 0.4;
+
+#[test]
+pub fn gaussian_window_short(){
+    // Expected values for a gaussian window with N = 16 and sigma = 0.4
+    let expected: [f32; 16] = [
+        0.04394,
+        0.09136,
+        0.17242,
+        0.29502,
+        0.45783,
+        0.64439,
+        0.82258,
+        0.95234,
+        1.0,
+        0.95234,
+        0.82258,
+        0.64439,
+        0.45783,
+        0.29502,
+        0.17242,
+        0.09136,
+    ];
+    
+    let window = gaussian_window::gaussian_window::<N_SHORT>(SIGMA);
+    assert_eq!(window.len(), N_SHORT);
+    
+    // make sure the window is correct to 4 decimal places (floating point error)
+    for i in 0..N_SHORT {
+        assert!((window[i] - expected[i]).abs() < 0.0001);
+    }
+}
+
+
+
