10eb2e5640761a7000e0a2936dd854a1f56f1022
Nicholas Ball <89214070+Nicholas-Ball@users.noreply.github.com>
2024-09-16 14:05:29 -0400

    Added Slider GUI element

--- examples/wave_generator/src/main.rs
+++ examples/wave_generator/src/main.rs
@@ -1,13 +1,17 @@
+use superdsp::gui::slider::Slider;
 use superdsp::gui::time_chart::TimeChart;
 use superdsp::objects::GUIExecutor;
 use superdsp::objects::object::{Bus, DSPObject};
 use superdsp::objects::wave_gen_time::WaveStepGenTime;
 
 fn main() {
-    let frequency = Bus::from_f32(200.0);
+    let mut frequency = Bus::from_f32(200.0);
     let amplitude = Bus::from_f32(1.0);
     let phase = Bus::from_f32(0.0);
     let sample_rate = Bus::from_f32(441.0);
+    
+    let mut slider = Slider::new(0.0, 100.0);
+    slider.set_bus(&mut frequency);
     
     let mut wave_step_gen = WaveStepGenTime::new(frequency, amplitude, phase, sample_rate);
     let mut chart = TimeChart::new();
@@ -15,5 +19,5 @@ fn main() {
     let s = wave_step_gen.get_bus();
     chart.set_bus(s);
 
-    GUIExecutor::run(vec![Box::new(chart)], Box::new(wave_step_gen));
+    GUIExecutor::run(vec![Box::new(chart), Box::new(slider)], Box::new(wave_step_gen));
 }
--- examples/wave_generator_complex_waterfall/src/main.rs
+++ examples/wave_generator_complex_waterfall/src/main.rs
@@ -1,17 +1,22 @@
+use superdsp::gui::slider::Slider;
 use superdsp::gui::waterfall::Waterfall;
 use superdsp::objects::GUIExecutor;
 use superdsp::objects::object::{Bus, DSPObject};
 use superdsp::objects::wave_gen_time_complex::WaveStepGenTimeComplex;
 
 fn main() {
-    let frequency = Bus::from_f32(200.0);
+    let mut frequency = Bus::from_f32(1.0);
     let amplitude = Bus::from_f32(1.0);
     let phase = Bus::from_f32(0.0);
     let sample_rate = Bus::from_f32(441.0);
-    
+
     let mut waterfall: Waterfall<256> = Waterfall::new();
+    let mut slider = Slider::new(0.0,100.0);
+    
+    slider.set_bus(&mut frequency);
+    
     let mut gen = WaveStepGenTimeComplex::new(frequency, amplitude, phase, sample_rate);
 
     waterfall.set_bus(gen.get_bus());
-    GUIExecutor::run(vec![Box::new(waterfall)], Box::new(gen))
+    GUIExecutor::run(vec![Box::new(waterfall), Box::new(slider)], Box::new(gen))
 }
