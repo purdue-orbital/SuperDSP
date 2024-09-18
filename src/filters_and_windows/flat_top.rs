302294565e9631879a91d49abdf130e9c6acf53e
Nicholas Ball <89214070+Nicholas-Ball@users.noreply.github.com>
2024-09-18 13:28:48 -0400

    Fixed the lack of spaces after equal signs

--- src/filters_and_windows/flat_top.rs
+++ src/filters_and_windows/flat_top.rs
@@ -4,11 +4,11 @@
 /// uses matlab constants, these constants will change the filter.
 /// i - Offset add offset maybe other var to take in?
 pub fn flat_top_window<const N: usize>(offset:usize) -> [f32; N] {
-    const A_0: f32 =0.21557895;
-    const A_1: f32 =0.41663158;
-    const A_2: f32 =0.277263158;
-    const A_3: f32 =0.083578947;
-    const A_4: f32 =0.006947368;
+    const A_0: f32 = 0.21557895;
+    const A_1: f32 = 0.41663158;
+    const A_2: f32 = 0.277263158;
+    const A_3: f32 = 0.083578947;
+    const A_4: f32 = 0.006947368;
     let mut filter: [f32;N] = [0.0; N];
     for i in 0..N{
       let n = (i as f32) - (offset as f32);
