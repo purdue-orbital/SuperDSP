515d48722cabea25e4098d96fa7b581eb247de5f
Nicholas Ball <89214070+Nicholas-Ball@users.noreply.github.com>
2024-09-18 13:16:38 -0400

    Fixed Flat Top

--- src/filters_and_windows/flat_top.rs
+++ src/filters_and_windows/flat_top.rs
@@ -12,7 +12,7 @@ pub fn flat_top_window<const N: usize>(offset:usize) -> [f32; N] {
     let mut filter: [f32;N] = [0.0; N];
     for i in 0..N{
       let n = (i as f32) - (offset as f32);
-       filter[i] = A_0 - A_1 * f32::cos(((2 as f32 * std::f32::consts::PI * n as f32)/N as f32)) + A_2 * f32::cos(((4 as f32 * std::f32::consts::PI * n as f32)/N as f32)) - A_3 * f32::cos(((6 as f32 * std::f32::consts::PI * n as f32)/N as f32)) + A_4 * f32::cos(((8 as f32 * std::f32::consts::PI * n as f32)/N as f32));
+       filter[i] = A_0 - A_1 * libm::cosf((2 as f32 * core::f32::consts::PI * n as f32)/N as f32) + A_2 * libm::cosf((4 as f32 * core::f32::consts::PI * n as f32)/N as f32) - A_3 * libm::cosf((6 as f32 * core::f32::consts::PI * n as f32)/N as f32) + A_4 * libm::cosf((8 as f32 * core::f32::consts::PI * n as f32)/N as f32);
     }
    filter
 }
