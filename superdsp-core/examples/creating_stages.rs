5743ecdfca66c05268594784d9ac860c91dec1d0
Nicholas Ball <nicholasball085@gmail.com>
2025-03-02 14:30:29 -0500

    Added better example

--- superdsp-core/examples/creating_stages.rs
+++ superdsp-core/examples/creating_stages.rs
@@ -7,7 +7,7 @@ fn hello_world_stage() {
     println!("Hello, world!");
 }
 
-fn get_information_stage_example(radio_information: Res<RadioInformation>) {
+fn get_information_stage_example(radio_information: Res<RadioInformation>, ) {
     let f = radio_information.frequency;
     let gain = radio_information.gain;
     
@@ -21,13 +21,27 @@ fn set_information_stage_example(mut radio_information: ResMut<RadioInformation>
     radio_information.taps = 10;
 }
 
+fn get_demo_resource(demo: Res<ResourceExample>){
+    println!("{}", demo.name);
+}
+
+pub struct ResourceExample{
+    pub name: String,
+}
+
 fn main() {
     let mut s = Scheduler::new();
     
     s.add_plugin(RadioCore);
     
+    s.add_resource(ResourceExample{
+        name: String::from("Hello, world"),
+    });
+    
     s.add_stage(Update, hello_world_stage);
     s.add_stage(Update, get_information_stage_example);
+    s.add_stage(Update, get_demo_resource);
+    
     s.add_stage(Startup, set_information_stage_example);
     
     s.setup();
